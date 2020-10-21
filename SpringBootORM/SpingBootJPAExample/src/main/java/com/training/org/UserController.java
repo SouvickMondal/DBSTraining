5e18f98163650825fcfc77a000d9c16c417a9e23
unknown <souvick@ALCHEMYCLOUD.CO.IN>
2020-10-22 00:21:23 +0800

    SpringBoot examples

---
+++ SpringBootORM/SpingBootJPAExample/src/main/java/com/training/org/UserController.java
@@ -0,0 +1,28 @@
+
+package com.training.org;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController    
+public class UserController   
+{    
+	@Autowired    
+	private UserService userService;     
+	@RequestMapping("/")    
+	public List<UserRecord> getAllUser()  
+	{    
+		return userService.getAllUsers();    
+	}       
+	@RequestMapping(value="/add-user", method=RequestMethod.POST)    
+	public void addUser(@RequestBody UserRecord userRecord)  
+	{    
+		userService.addUser(userRecord);    
+	}       
+}        
+
